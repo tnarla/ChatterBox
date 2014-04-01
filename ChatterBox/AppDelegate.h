f8725eabdf02f37a121033d90f82b1c0b65b7a14
Trushitha Narla <tnarla@Trushithas-MacBook-Pro.local>
2014-03-31 20:37:17 -0500

    Initial Commit

---
+++ ChatterBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChatterBox
+//
+//  Created by Trushitha Narla on 3/31/14.
+//  Copyright (c) 2014 Trushitha Narla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
